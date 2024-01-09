a545b97ce81dcde77c72957a3f87513374683370
Aleksey Vityazev <alexeyvityazev@gmail.com>
2024-01-09 15:19:59 +0300

    Parameterized class

---
+++ src/main/ParametrizedClass.java
@@ -0,0 +1,24 @@
+public class ParametrizedClass {
+    public static void main(String[] args) {
+        Info<String> info1 = new Info<>("Privet");
+        System.out.println(info1);
+        Info<Integer> info2 = new Info<>(20);
+        System.out.println(info2);
+    }
+
+}
+
+class Info<T>{
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "value=" + value +
+                '}';
+    }
+}
